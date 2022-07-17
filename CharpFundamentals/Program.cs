fa959b4e6ad6da82889398c5c9cd3f1b20b3b5a0
uvasree <uvasree11@gmail.com>
2022-07-17 07:59:05 +0530

    added first project

---
+++ CharpFundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
