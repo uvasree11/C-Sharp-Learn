c0c8747f71c227ec0acbe81dcd3bcac35643bb2e
uvasree <uvasree11@gmail.com>
2022-07-26 09:07:45 +0530

    linq group by

--- CharpFundamentals/Collections/LinqGroupBy.cs
+++ CharpFundamentals/Collections/LinqGroupBy.cs
@@ -1,29 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CharpFundamentals.Collections
 {
-    class LinqGroupBy
+    public class LinqGroupBy
     {
-        public class Student{
-            public int ID { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public List<int> ExamScores { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        
 
-            public Student(int id,string firstname,string lastname,List<int> examscores)
-            {
-                this.ID = id;
-                this.FirstName = firstname;
-                this.LastName = lastname;
-                this.ExamScores = examscores;
-            }
-            List<Student> student = new List<Student>()
-            {
-                new Student(1, "Raja", "Raj",new List<int>{24,10,35}),
-                new Student(2, "Kumar","Kum", new List<int>{20,15,20 })
-            };
+
+        public LinqGroupBy(int id,string name,int mark)
+        {
+            Id = id;
+            Name = name;
+            Mark = mark;
+        }
+
     }
+    public class student
+    {
+        public List<LinqGroupBy> StdList { get; set; }
+        public student()
+        {
+            StdList = new List<LinqGroupBy>()
+            {
+                new LinqGroupBy(1,"uvi",100),
+                new LinqGroupBy(2,"sree",100),
+                new LinqGroupBy(3,"uvi",70)
+            };
+        }
+
+        public void linqGB()
+        {
+            var listStd = from std in StdList
+                          group std by std.Name into result
+                          orderby result.Key
+                          select result;
+            foreach (var lstd in listStd)
+            {
+
+                foreach (var student in lstd)
+                {
+
+                    Console.WriteLine(student.Name);
+                }
+            }
+
+
+        }
+
+        public void grpbyValue()
+        {
+            var lstgbv = from std in StdList
+                         group std by std.Name[0];
+                        
+            foreach(var lst in lstgbv)
+            {
+                Console.WriteLine($"Key: { lst.Key}");
+                foreach (var lststd in lst)
+                {
+                    Console.WriteLine($"Name: {lststd.Name}");
+                }
+            }
+        }
+        public void grpByComparison()
+        {
+            var lstgbr = from std in StdList
+                         group new { std.Id, std.Name } by std.Mark > 50 into result
+                         select result;
+            foreach (var lst in lstgbr)
+            {
+                Console.WriteLine($"Key: { lst.Key}");
+                foreach (var lststd in lst)
+                {
+                    Console.WriteLine($"Name: {lststd.Name}");
+                    Console.WriteLine($"Name: {lststd.Id}");
+                }
+            }
+        }
+        public void grpByRange()
+        {
+            var lstgbr = from std in StdList
+                         group new { std.Id, std.Name } by (double)std.Mark/3 > 0 ? (int)std.Mark/3 / 10 : 0 into result
+                         select result;
+            foreach (var lst in lstgbr)
+            {
+                Console.WriteLine($"Key: { lst.Key}");
+                foreach (var lststd in lst)
+                {
+                    Console.WriteLine($"Name: {lststd.Name}");
+                    Console.WriteLine($"id: {lststd.Id}");
+                }
+            }
+        }
     }
 }
--- CharpFundamentals/Program.cs
+++ CharpFundamentals/Program.cs
@@ -10,8 +10,13 @@ namespace CharpFundamentals
             // Console.WriteLine("Hello World!");
             //Linq1 l1=new Linq1();
             // l1.LinqLearning();
-            LinqToXml lxml = new LinqToXml();
-            lxml.readLinqToXML();
+            //LinqToXml lxml = new LinqToXml();
+            //lxml.readLinqToXML();
+            student std = new student();
+            //std.linqGB();
+            //std.grpbyValue();
+            //std.grpByComparison();
+            std.grpByRange();
 
         }
     }
